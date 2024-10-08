60c7938763593de9522b6d3a5170c8aefaea6330
morshedulmunna <morshedulmunna1@gmail.com>
2024-10-08 11:32:59 +0600

    Project init

---
+++ services/inventory/src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+// 404 handler
+app.use((_req: express.Request, res: express.Response) => {
+  res.status(404).send("Not found");
+});
+
+// Error handler
+app.use(
+  (
+    err: Error,
+    _req: express.Request,
+    res: express.Response,
+    _next: express.NextFunction
+  ) => {
+    res.status(500).send(`Internal server error: ${err.message}`);
+  }
+);
+
+const PORT = process.env.PORT || 3000;
+const serviceName = process.env.SERVICE_NAME || "inventory";
+
+app.listen(PORT, () => {
+  console.log(`${serviceName} is listening on port ${PORT}`);
+});
